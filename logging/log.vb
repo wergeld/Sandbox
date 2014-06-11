bf12bda20fee4d40c3742b93cfd6e2eb0f73ac94
shorqubit <crsmit11@mail.usf.edu>
2014-06-11 10:29:37 -0400

    Update log.vb

--- logging/log.vb
+++ logging/log.vb
@@ -1,11 +1,11 @@
-Public NotInheritable Class MySingleton
+Public NotInheritable Class Log
     Private Shared ReadOnly _instance As New Lazy(Of MySingleton)(Function() New
-        MySingleton(), System.Threading.LazyThreadSafetyMode.ExecutionAndPublication)
+        Log(), System.Threading.LazyThreadSafetyMode.ExecutionAndPublication)
 
     Private Sub New()
     End Sub
 
-    Public Shared ReadOnly Property Instance() As MySingleton
+    Public Shared ReadOnly Property Instance() As Log
         Get
             Return _instance.Value
         End Get
