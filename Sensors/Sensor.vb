3d1b485ba31417683db7806db49a33a7ebcbebbf
wergeld <wergeld@gmail.com>
2014-06-11 10:13:08 -0400

    Create Sensor.vb

---
+++ Sensors/Sensor.vb
@@ -0,0 +1,31 @@
+Public Class Sensor
+  Public ID As Integer
+  Public location As String
+  Public available As boolean
+  
+  
+  Private Function getLocation (ByVal ID) As String
+    Try
+      Result = dbConnection(run usp_getLocationByID)
+      
+      If Result > 0 Then
+        Return Result
+      End
+    Catch
+     Throw
+    End Try
+  End Function
+  
+    Private Function isAvailable (ByVal ID) As Boolean
+    Try
+      Result = dbConnection(run usp_getAvailabilityByID)
+      
+      If Not Result Is Nothing Then
+        Return Result
+      End
+    Catch
+     Throw
+    End Try
+  End Function
+  
+End Class
